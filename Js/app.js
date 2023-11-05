573f77a680da06c2ca4786b0528081414f481804
Olliehub <charlibossubom@gmail.com>
2023-11-05 18:37:23 +0100

    ALX Capstone Project on Personal Portfolio Website.

--- Js/app.js
+++
@@ -1,7 +0,0 @@
-var typed= new typed(".text",{
-    Strings:["Web Developer", "Software Engineer", "Freelancer", "Recording Artiste"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
